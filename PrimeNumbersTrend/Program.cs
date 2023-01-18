da295f3bd429455b3dc8796bb5722e3489cd2d0a
mrsharm <musharm@microsoft.com>
2023-01-17 19:11:16 -0800

    Adjusted the trending to the new output format

--- PrimeNumbersTrend/Program.cs
+++ PrimeNumbersTrend/Program.cs
@@ -1,180 +1,138 @@
-﻿using System;
+﻿using CommandLine;
+using DataModel;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Numerics;
 using System.Text;
-using System.Threading;
-//using System.Linq;
+
+public class Options
+{
+    [Option('p', "path", Required = true, HelpText = "Path to Prime Number Trend.")]
+    public string Path { get; set; }
+
+    [Option('i', "maxInputSize", Required = true, HelpText = "Max input size.")]
+    public int MaxInputSize { get; set; }
+
+    [Option('c', "maxComplexity", Required = true, HelpText = "Max Complexity.")]
+    public int MaxComplexity { get; set; }
+
+    [Option('t', "maxThreads", Required = false, HelpText = "Max Number of Threads.")]
+    public int? MaxThreads { get; set; }
+}
 
 public class PrimeNumbersTrend {
 
     private static ProcessStartInfo startInfo;
 
-    public static void Main(String[] args) {
-        if ((args.Length != 3) && (args.Length != 4)) {
-            Console.WriteLine("Usage: PrimeNumbersTrend.exe <Path> <maxInputSize> <maxComplexity> <maxThreads>?");
-            Console.WriteLine("<Path>: Full path to PrimeNumbers.exe");
-
-            Console.WriteLine("<maxInputSize>: Max Input size.");
-            Console.WriteLine("<maxComplexity>: Max Complexity.");
-            Console.WriteLine("<maxThreads>: Max Threads.");
-            Environment.Exit(1);
-        }
-
-        string primeNumbersExe = args[0];
-        if (!File.Exists(primeNumbersExe)) {
-            Console.WriteLine($"{primeNumbersExe} not present.");
-            Environment.Exit(1);
-        }
-
-        int maxInputSize = Int32.Parse(args[1]);
-        int maxComplexity = Int32.Parse(args[2]);
-        int maxThreads = 1;
-        bool useDefaultThreads = true;
-        if (args.Length > 3) {
-            int inputThread = Int32.Parse(args[3]);
-            // Ensure we process only valid thread count
-            if (inputThread > 1) {
-                maxThreads = inputThread;
-                useDefaultThreads = false;
-            }
-        }
-
-        startInfo = new ProcessStartInfo() {
-            FileName = primeNumbersExe,
-            Arguments = "",
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-        };
-
-        StringBuilder results = new StringBuilder();
-        results.Append("inputSize|complex|thread");
-        results.Append("|iters_per_number");
-        results.Append("|hardwait_per_number");
-        results.Append("|softwait_per_number");
-        results.Append("|hardWaitWakeup_per_number");
-        results.Append("|softWaitWakeup_per_number");
-        results.Append("|iters_number_allthreads");
-        results.Append("|hardwait_number_allthreads");
-        results.Append("|softwait_number_allthreads");
-        results.Append("|hardWaitWakeup_number_allthreads");
-        results.Append("|softWaitWakeup_number_allthreads");
-        results.Append("|iters_thread_allnumbers");
-        results.Append("|hardwait_thread_allnumbers");
-        results.Append("|softwait_thread_allnumbers");
-        results.Append("|hardWaitWakeup_thread_allnumbers");
-        results.Append("|softWaitWakeup_thread_allnumbers");
-        results.Append("|ticks");
-        results.Append("|totalTime");
-        results.AppendLine();
-        int columns = results.ToString().Count(c => c == '|');
-        for (int col = 0; col <= columns; col++) {
-            results.Append("--|");
-        }
-        results.AppendLine();
-
-        int progressIteration = 0;
-        int totalIterations = 5 * maxInputSize * (maxComplexity+1);
-        if (!useDefaultThreads) {
-            totalIterations *= maxThreads;
-        }
-        int percentComplete = 5;
-        Console.Write("Progress: 0%");
-        for (int inputSize = 1; inputSize <= maxInputSize; inputSize++) {
-            for (int complex = 0; complex <= maxComplexity; complex++) {
-                for (int thread = 1; thread <= maxThreads; thread++) {
-                    List<long> iters_pernumber = new();
-                    List<long> hardwait_pernumber = new();
-                    List<long> softwait_pernumber = new();
-                    List<long> hardWaitWakeup_pernumber = new();
-                    List<long> softWaitWakeup_pernumber = new();
-                    List<long> iters_number = new();
-                    List<long> hardwait_number = new();
-                    List<long> softwait_number = new();
-                    List<long> hardWaitWakeup_number = new();
-                    List<long> softWaitWakeup_number = new();
-                    List<long> iters_thread = new();
-                    List<long> hardwait_thread = new();
-                    List<long> softwait_thread = new();
-                    List<long> hardWaitWakeup_thread = new();
-                    List<long> softWaitWakeup_thread = new();
-                    List<long> ticks = new();
-                    List<long> totalTime = new();
-
-                    // Retry 5 times and take average.
-                    for (int iter = 0; iter < 5; iter++) {
-                        var result = RunAndGetResult(iter, inputSize, complex, useDefaultThreads ? 0 : thread);
-                        if (IsEmpty(result)) continue;
-
-                        int col = 3;
-                        iters_pernumber.Add(result[col++]);
-                        hardwait_pernumber.Add(result[col++]);
-                        softwait_pernumber.Add(result[col++]);
-                        hardWaitWakeup_pernumber.Add(result[col++]);
-                        softWaitWakeup_pernumber.Add(result[col++]);
-                        iters_number.Add(result[col++]);
-                        hardwait_number.Add(result[col++]);
-                        softwait_number.Add(result[col++]);
-                        hardWaitWakeup_number.Add(result[col++]);
-                        softWaitWakeup_number.Add(result[col++]);
-                        iters_thread.Add(result[col++]);
-                        hardwait_thread.Add(result[col++]);
-                        softwait_thread.Add(result[col++]);
-                        hardWaitWakeup_thread.Add(result[col++]);
-                        softWaitWakeup_thread.Add(result[col++]);
-                        ticks.Add(result[col++]);
-                        totalTime.Add(result[col]);
-                        Debug.Assert(col == columns);
-
-                        progressIteration++;
-                        int percent = (progressIteration * 100) / totalIterations;
-                        if (percent == percentComplete) {
-                            Console.Write($" {percentComplete}%");
-                            percentComplete += 5;
-                        }
-                    }
-
-                    results.Append($"{inputSize}|{complex}|{thread}");
-                    results.Append($"|{iters_pernumber.Average()}");
-                    results.Append($"|{hardwait_pernumber.Average()}");
-                    results.Append($"|{softwait_pernumber.Average()}");
-                    results.Append($"|{hardWaitWakeup_pernumber.Average()}");
-                    results.Append($"|{softWaitWakeup_pernumber.Average()}");
-                    results.Append($"|{iters_number.Average()}");
-                    results.Append($"|{hardwait_number.Average()}");
-                    results.Append($"|{softwait_number.Average()}");
-                    results.Append($"|{hardWaitWakeup_number.Average()}");
-                    results.Append($"|{softWaitWakeup_number.Average()}");
-                    results.Append($"|{iters_thread.Average()}");
-                    results.Append($"|{hardwait_thread.Average()}");
-                    results.Append($"|{softwait_thread.Average()}");
-                    results.Append($"|{hardWaitWakeup_thread.Average()}");
-                    results.Append($"|{softWaitWakeup_thread.Average()}");
-                    results.Append($"|{ticks.Average()}");
-                    results.Append($"|{totalTime.Average()}");
-                    results.AppendLine();
+    public static void Main(String[] args) 
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                bool useDefaultThreads = true;
+                if (string.IsNullOrEmpty(o.Path) || !File.Exists(o.Path))
+                {
+                    Console.WriteLine($"{o.Path} not present.");
+                    Environment.Exit(1);
                 }
 
-            }
-        }
-        Console.WriteLine();
-        Console.WriteLine("-------------------------");
-        Console.WriteLine(results.ToString());
+                if (!o.MaxThreads.HasValue)
+                {
+                    o.MaxThreads = 1;
+                }
+                else
+                {
+                    useDefaultThreads = false;
+                }
+
+                startInfo = new ProcessStartInfo() {
+                    FileName = o.Path,
+                    Arguments = "",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                };
+
+                StringBuilder results = new StringBuilder();
+                results.Append("inputSize|complexity|thread");
+                results.Append("| HT|Affinity|Input_count");
+                results.Append("|join_type");
+                results.Append("|Spin_count");
+                results.Append("|Thread_count");
+                results.Append("|Soft Wait : Total");
+                results.Append("|Soft Wait: #/input");
+                results.Append("|Soft Wait: Iterations/wait");
+                results.Append("|Soft Wait: Spin time / wait");
+                results.Append("|Soft Wait: wakeup latency / wait");
+                results.Append("|Hard Wait : Total");
+                results.Append("|Hard Wait: #/input");
+                results.Append("|Hard Wait: Iterations/wait");
+                results.Append("|Hard Wait: Spin time / wait");
+                results.Append("|Hard Wait: wakeup latency / wait");
+                results.Append("|Total cycles spinning ");
+                results.Append("|Cycles spin per thread");
+                results.Append("|Elapsed time");
+                results.Append("|Elapsed cycles");
+                results.AppendLine();
+                int columns = results.ToString().Count(c => c == '|');
+                for (int col = 0; col <= columns; col++) {
+                    results.Append("--|");
+                }
+                results.AppendLine();
+
+                int progressIteration = 0;
+                int totalIterations = 5 * o.MaxInputSize * (o.MaxComplexity + 1);
+                if (!useDefaultThreads) {
+                    totalIterations *= o.MaxThreads.Value;
+                }
+                int percentComplete = 5;
+                Console.Write("Progress: 0%");
+                for (int inputSize = 1; inputSize <= o.MaxInputSize; inputSize++) {
+                    for (int complex = 0; complex <= o.MaxComplexity; complex++) {
+                        for (int thread = 1; thread <= o.MaxThreads; thread++) {
+
+                            List<ResultItem> iterations = new();
+
+                            // Retry 5 times and take average.
+                            for (int iter = 0; iter < 5; iter++) {
+                                ResultItem? result = RunAndGetResult(iter, inputSize, complex, useDefaultThreads ? 0 : thread);
+                                if (result == null) continue;
+
+                                iterations.Add(result);
+
+                                progressIteration++;
+                                int percent = (progressIteration * 100) / totalIterations;
+                                if (percent == percentComplete) {
+                                    Console.Write($" {percentComplete}%");
+                                    percentComplete += 5;
+                                }
+                            }
+
+                            results.Append($"{inputSize}|{complex}|{thread}");
+                            results.Append($"|{ResultItem.ConstructAveragedResultItems(iterations).ToMarkDownRow()}");
+                            results.AppendLine();
+                        }
+
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("-------------------------");
+                Console.WriteLine(results.ToString());
+            });
     }
 
 
-    private static List<long> RunAndGetResult(int iter, int inputSize, int complexity, int threads) {
-        List<long> result = null;
+    private static ResultItem? RunAndGetResult(int iter, int inputSize, int complexity, int threads) {
         StringBuilder stdoutBuilder = new StringBuilder();
         StringBuilder stderrBuilder = new StringBuilder();
+        ResultItem? result = null;
 
-        startInfo.Arguments = $"{inputSize} {complexity} ";
+        startInfo.Arguments = $"--input_count {inputSize} --complexity {complexity} ";
 
         if (threads != 0) {
-            startInfo.Arguments += $" {threads}";
+            startInfo.Arguments += $" --thread_count {threads}";
         }
 
         string output = "", error = "";
@@ -188,7 +146,6 @@ public class PrimeNumbersTrend {
                 if (!string.IsNullOrEmpty(output)) {
                     stdoutBuilder.AppendLine(output);
                 }
-                //Console.WriteLine(output);
             }
         };
         process.ErrorDataReceived += (sender, args) => {
@@ -219,24 +176,17 @@ public class PrimeNumbersTrend {
                     }
                     string resultOutput = outputLine.Replace("OUT] ", string.Empty);
                     string[] parsedOutput = resultOutput.Split('|', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-                    result = parsedOutput.Select(x => long.Parse(x)).ToList();
+                    result = new ResultItem(parsedOutput);
+                    return result;
                 }
             }
-            else {
-                Console.WriteLine("*** Exitcode " + process.ExitCode);
-            }
         }
 
-        if (IsEmpty(result)) {
-            Console.WriteLine("[OUT]: ");
-            Console.WriteLine(output);
-            Console.WriteLine("[ERR]: ");
-            Console.WriteLine(error);
-        }
+        Console.WriteLine("*** Exitcode " + process?.ExitCode);
+        Console.WriteLine("[OUT]: ");
+        Console.WriteLine(output);
+        Console.WriteLine("[ERR]: ");
+        Console.WriteLine(error);
         return result;
     }
-
-    private static bool IsEmpty(List<long> result) {
-        return ((result == null) || (result.Count == 0));
-    }
-}
+} 
---
+++ PrimeNumbersTrend/ResultItem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataModel
+{
+    public sealed class ResultItem
+    {
+        // HT|affinity|5|3|t_join_pause|128000|5|16|     0.800|9170|2122538|911124|0|     0.000|0|0|0|33960602|6792120|3|11541562
+        public ResultItem(string[] parsedOutput)
+        {
+            HT = parsedOutput[0];
+            Affinity = parsedOutput[1];
+            InputCount = Convert.ToInt32(parsedOutput[2]);
+            Complexity = Convert.ToInt32(parsedOutput[3]);
+            JoinType = parsedOutput[4];
+            SpinCount = Convert.ToInt64(parsedOutput[5]);
+            ThreadCount = Convert.ToInt64(parsedOutput[6]);
+            SoftWaitTotal = Convert.ToInt64(parsedOutput[7]);
+            SoftWaitNumPerInput = Convert.ToDouble(parsedOutput[8].Contains("nan") ? double.NaN : parsedOutput[8]);
+            SoftWaitIterationsPerWait = Convert.ToDouble(parsedOutput[9]);
+            SoftWaitSpinTimePerWait = Convert.ToDouble(parsedOutput[10]);
+            SoftWaitWakeupLatencyPerWait = Convert.ToDouble(parsedOutput[11]);
+            HardWaitTotal = Convert.ToInt64(parsedOutput[12]);
+            HardWaitNumPerInput = Convert.ToDouble(parsedOutput[13].Contains("nan") ? double.NaN : parsedOutput[13]);
+            HardWaitIterationsPerWait = Convert.ToDouble(parsedOutput[14]);
+            HardWaitSpinTimePerWait = Convert.ToDouble(parsedOutput[15]);
+            HardWaitWakeupLatencyPerWait = Convert.ToDouble(parsedOutput[16]);
+            CyclesSpinPerThread = Convert.ToDouble(parsedOutput[17]);
+            ElapsedTime = Convert.ToDouble(parsedOutput[18]);
+            ElapsedCycles = Convert.ToDouble(parsedOutput[19]);
+        }
+
+        private ResultItem() { }
+         
+        public string HT { get; set; }
+        public string Affinity { get; set; }
+        public long InputCount { get; set; }
+        public int Complexity { get; set; }
+        public string JoinType { get; set; }
+        public long SpinCount { get; set; }
+        public long ThreadCount { get; set; }
+        public long SoftWaitTotal { get; set; }
+        public double SoftWaitNumPerInput { get; set; }
+        public double SoftWaitIterationsPerWait { get; set; }
+        public double SoftWaitSpinTimePerWait { get; set; }
+        public double SoftWaitWakeupLatencyPerWait { get; set; }
+        public long HardWaitTotal { get; set; }
+        public double HardWaitNumPerInput { get; set; }
+        public double HardWaitIterationsPerWait { get; set; }
+        public double HardWaitSpinTimePerWait { get; set; }
+        public double HardWaitWakeupLatencyPerWait { get; set; }
+        public double CyclesSpinPerThread { get; set; }
+        public double ElapsedTime { get; set; }
+        public double ElapsedCycles { get; set; }
+
+        public string ToMarkDownRow()
+            => $"{HT}|{Affinity}|{InputCount}|{Complexity}|{JoinType}|{SpinCount}|{ThreadCount}|{SoftWaitTotal}|{SoftWaitNumPerInput}|{SoftWaitIterationsPerWait}|{SoftWaitSpinTimePerWait}|{SoftWaitWakeupLatencyPerWait}|{HardWaitTotal}|{HardWaitNumPerInput}|{HardWaitIterationsPerWait}|{HardWaitSpinTimePerWait}|{HardWaitWakeupLatencyPerWait}|{CyclesSpinPerThread}|{ElapsedTime}|{ElapsedCycles}";
+        public string ToCSVRow()
+            => $"{HT},{Affinity},{InputCount},{Complexity},{JoinType},{SpinCount},{ThreadCount},{SoftWaitTotal},{SoftWaitNumPerInput},{SoftWaitIterationsPerWait},{SoftWaitSpinTimePerWait},{SoftWaitWakeupLatencyPerWait},{HardWaitTotal},{HardWaitNumPerInput},{HardWaitIterationsPerWait},{HardWaitSpinTimePerWait},{HardWaitWakeupLatencyPerWait},{CyclesSpinPerThread},{ElapsedTime},{ElapsedCycles}";
+
+        public static ResultItem ConstructAveragedResultItems(IReadOnlyList<ResultItem> others)
+        {
+            ResultItem result = new()
+            {
+                HT         = others[0].HT,
+                Affinity   = others[0].Affinity,
+                InputCount = others[0].InputCount,
+            };
+
+            result.SpinCount   = (long)others.Average(o => o.SpinCount);
+            result.ThreadCount = (long)others.Average(o => o.ThreadCount);
+
+            result.SoftWaitTotal = (long)others.Average(o => o.SoftWaitTotal);
+            result.SoftWaitNumPerInput = others.Average(o => o.SoftWaitNumPerInput);
+            result.SoftWaitIterationsPerWait = others.Average(o => o.SoftWaitIterationsPerWait);
+            result.SoftWaitSpinTimePerWait = others.Average(o => o.SoftWaitSpinTimePerWait);
+            result.SoftWaitWakeupLatencyPerWait = others.Average(o => o.SoftWaitWakeupLatencyPerWait);
+
+            result.HardWaitTotal = (long)others.Average(o => o.HardWaitTotal);
+            result.HardWaitNumPerInput = others.Average(o => o.HardWaitNumPerInput);
+            result.HardWaitIterationsPerWait = others.Average(o => o.HardWaitIterationsPerWait);
+            result.HardWaitSpinTimePerWait = others.Average(o => o.HardWaitSpinTimePerWait);
+            result.HardWaitWakeupLatencyPerWait = others.Average(o => o.HardWaitWakeupLatencyPerWait);
+
+            result.CyclesSpinPerThread = others.Average(o => o.CyclesSpinPerThread);
+            result.ElapsedTime = others.Average(o => o.ElapsedTime);
+            result.ElapsedCycles = others.Average(o => o.ElapsedCycles);
+
+            return result;
+        }
+    }
+}
